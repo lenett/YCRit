654c8ef6b9232b811bd3727c1e27a5b68fb801e9
lenett <84128865+lenett@users.noreply.github.com>
2022-10-10 00:05:59 +0300

    repair smth

---
+++ EvaBot/Web/EvaWeb/Components/NavigationBarViewComponent.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EvaWeb.Models;
+
+namespace EvaWeb.Components
+{
+    public class NavigationBarViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            List<NavigationBarLink> navibarLink = InicializeNavbar();
+
+
+            return View(navibarLink);
+        }
+
+        private List<NavigationBarLink> InicializeNavbar()
+        {
+            List<NavigationBarLink> navibarLink = new List<NavigationBarLink>();
+            navibarLink.Add(new NavigationBarLink { ClassParametrs = "nav-link text-white", Image = "#home", Link = "/#", Text = "Главная", StartLi =true});
+            navibarLink.Add(new NavigationBarLink { ClassParametrs = "nav-link text-white", Image = "#table", Link = "/library/books/index", Text = "Библиотека", StartLi = true });
+            navibarLink.Add(new NavigationBarLink { ClassParametrs = "nav-link text-white", Image = "#grid", Link = "/identity/account/login", Text = "Словарь", StartLi = true });
+            //navibarLink.Add(new NavibarLink { classParametrs = "collapsible", image = "#home", link = "#", text = "Администрирование", opLi = true });
+            //navibarLink.Add(new NavibarLink { classParametrs = "", image = "#speedometer2", link = "/identity/account/login", text = "Пользователи", endLi = true, content=true });
+
+            return navibarLink;
+        }
+    }
+}
---
+++ EvaBot/Web/EvaWeb/Models/NavigationBarLink.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace EvaWeb.Models
+{
+    public struct NavigationBarLink
+    {
+        public string Text { get; set; }
+        public string Link { get; set; }
+        public string Image { get; set; }
+        public string ClassParametrs { get; set; }
+        public bool StartLi { get; set; }
+        public bool EndLi { get; set; }
+        public bool Content { get; set; }
+
+        public string GetStartLi()
+        {
+
+            if (StartLi) return "<li>";
+            else return "";
+        }
+        public string GetEndLi()
+        {
+            string res = "</li>";
+            if (StartLi) return res.ToString();
+            else return "";
+        }
+
+    }
+    
+
+
+
+}
